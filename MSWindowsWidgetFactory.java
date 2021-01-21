152288cebabcbcc20fcbd2a63ae789625abbf974
Baynor  Erica <baynore@student2.cs.appstate.edu>
2021-01-21 16:54:42 -0500

    first commit

---
+++ MSWindowsWidgetFactory.java
@@ -0,0 +1,11 @@
+
+public class MSWindowsWidgetFactory implements AbstractWidgetactory {
+
+    public Window createWindow() {
+    
+        MSWindow window = new MSWindow();
+        return window;
+    
+    }
+
+}
